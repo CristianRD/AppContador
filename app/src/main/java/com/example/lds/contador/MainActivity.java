19108c0637692508d9b093a85b78adc51d00282d
Laboratorio de Software <lds@unac.edu.co>
2016-08-08 16:53:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lds/contador/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lds.contador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button click;
+    private TextView TextViewcontador;
+
+    private int contador = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextViewcontador = (TextView) findViewById(R.id.TextView);
+        click = (Button) findViewById(R.id.buttonclick);
+        click.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                contador++;
+                if(contador == 1) {
+
+                    TextViewcontador.setText(contador + "Click");
+                }else{
+                    TextViewcontador.setText(contador + "Clicks");
+                }
+            }
+        });
+
+    }
+}
